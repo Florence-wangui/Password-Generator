d93cdd88efb89e92056977621d186c4e30301cfb
Florence Nyokabi Wangui <99530400+Florence-wangui@users.noreply.github.com>
2022-06-28 17:03:03 +0300

    password generator

--- solution/password_generator.cs
+++ solution/password_generator.cs
@@ -1,2 +1,33 @@
 using System;
-
+using System.Text;
+ 
+public class Example
+{
+    public static string GetRandomPassword(int length)
+    {
+        const string chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+ 
+        StringBuilder sb = new StringBuilder();
+        Random rnd = new Random();
+ 
+        for (int i = 0; i < length; i++)
+        {
+            int index = rnd.Next(chars.Length);
+            sb.Append(chars[index]);
+        }
+ 
+        return sb.ToString();
+    }
+ 
+    public static void Main()
+    {
+        int length = 10;
+ 
+        string password = GetRandomPassword(length);
+        Console.WriteLine(password);
+    }
+}
+ 
+/*
+    Output: 3Dn6V7dK48
+*/
